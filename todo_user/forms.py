6f3c4261a334ff74ac64cc63f0e66e4ac61b2453
johnwonderbread <tyler.johnathan@gmail.com>
2020-03-30 12:10:50 +0000

    bug fixes todo_user

--- todo_user/forms.py
+++ todo_user/forms.py
@@ -31,7 +31,7 @@ class SignUpForm(UserCreationForm):
 
 class EditProfileForm(UserChangeForm):
 
-    password = forms.CharField(label = "", max_length=50, widget=forms.TextInput(attrs={'type':'hidden'}))
+    password = forms.CharField(label = "", widget=forms.TextInput(attrs={'type':'hidden'}))
     username = forms.CharField(label = "", max_length=50, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
     email = forms.EmailField(label = "", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
     first_name = forms.CharField(label = "", max_length=50, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
