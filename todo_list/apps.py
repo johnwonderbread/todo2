4f09ac726a9afe7c9bd06da0c190e14384f8a774
John Tyler <johntyler@Johns-MacBook-Pro-2.local>
2020-03-30 21:42:10 -0400

    user specific todo list

--- todo_list/apps.py
+++ todo_list/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class TodoListConfig(AppConfig):
     name = 'todo_list'
--- todo_list/views.py
+++ todo_list/views.py
@@ -3,20 +3,27 @@ from .models import List
 from .forms import ListForm
 from django.contrib import messages
 from django.http import HttpResponseRedirect
+from django.conf import settings
 
 def home(request): 
-	if request.method == 'POST': 
-		form = ListForm(request.POST or None)
+	if not request.user.is_authenticated:
+		return redirect('login')
 
-		if form.is_valid():
-			form.save()
-			all_items = List.objects.all
-			messages.success(request, ('Item has been successfully added to the list!'))
+	else: 
+		if request.method == 'POST': 
+			form = ListForm(request.POST or None)
+			
+			if form.is_valid():
+				fs=form.save(commit=False)
+				fs.user=request.user
+				fs.save()
+				context={'form':form, 'all_items':List.objects.all}
+				messages.success(request, ('Item has been successfully added to the list!'))
+				return render(request, 'todo_list/home.html', context)
+		
+		else:
+			all_items = List.objects.all 
 			return render(request, 'todo_list/home.html', {'all_items': all_items})
-	
-	else:
-		all_items = List.objects.all 
-		return render(request, 'todo_list/home.html', {'all_items': all_items})
 
 def about(request):
 	context = {'first_name': 'John', 'last_name': 'Tyler'}
