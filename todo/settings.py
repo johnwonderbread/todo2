7a85fd9b43beee2247a644b4bf4c8f13c2f04d1d
johnwonderbread <tyler.johnathan@gmail.com>
2020-04-12 18:45:14 +0000

    sync

--- todo/settings.py
+++ todo/settings.py
@@ -118,10 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/'
+STATIC_URL = '/home/johnwonderbread/todo/static/'
+STATIC_ROOT = '/home/johnwonderbread/todo/static'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    os.path.join(BASE_DIR, "/home/johnwonderbread/todo/static"),
 ]
 
