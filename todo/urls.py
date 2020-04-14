82da5c87c6730667ede502efd1a7fac4a45a38a8
johnwonderbread <tyler.johnathan@gmail.com>
2020-04-14 03:34:22 +0000

    setup for react frontend

--- todo/urls.py
+++ todo/urls.py
@@ -4,10 +4,10 @@ from todo_list import views
 from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('todo_list.urls')),
-#    re_path(r'^api/todo_list/$', views.todo_list),
-#    re_path(r'^api/todo_list/([0-9]+)$', views.todo_detail),
-    path('user/', include('todo_user.urls')),
+ #  path('admin/', admin.site.urls),
+ #  path('', include('todo_list.urls')),
+    re_path(r'^todo_list/$', views.todo_list),
+    re_path(r'^todo_list/([0-9]+)$', views.todo_detail),
+ #  path('user/', include('todo_user.urls')),
 ]
 
